fd3dfbf8bb598eccd9f9d27cdd063fe09ec52adc
Kaiwen <gaoxkaiwen@gmail.com>
2021-04-29 00:20:24 -0300

    Open app directly at youtube sharing and incomplete directory chooser

--- app/src/main/java/com/example/youtubedownloader/MainActivity.java
+++ app/src/main/java/com/example/youtubedownloader/MainActivity.java
@@ -17,10 +17,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -43,15 +45,21 @@ public class MainActivity extends AppCompatActivity {
     public static final int progress_bar_type = 0;
 
     EditText linkText;
-    Button savebttn;
-    Button findButton;
+    Button findBttn;
+    Button saveButton;
     TextView textView;
+    Spinner videosResolution;
+    ImageButton searchDirBttn;
+    TextView directoryText;
+
     private VideoDetails details;
     private String videoURL;
     private String pathToDownload;
+    private AudioVideoFormat audioVideoFormat;
+    private List<AudioVideoFormat> videoWithAudioFormats;
 
-    Spinner videosResolution;
-    final AudioVideoFormat[] qualityPosition = new AudioVideoFormat[1];
+
+    final int[] qualityPosition = new int[1];
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -62,22 +70,35 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
 
         linkText = ((EditText)findViewById(R.id.linkText));
-        savebttn = ((Button)findViewById(R.id.saveBttn));
+        findBttn = ((Button)findViewById(R.id.findBttn));
         textView = ((TextView)findViewById(R.id.textView));
         videosResolution = ((Spinner)findViewById(R.id.videosResolution));
-        findButton = ((Button)findViewById(R.id.findButton));
+        saveButton = ((Button)findViewById(R.id.saveBttn));
+        searchDirBttn = ((ImageButton)findViewById(R.id.searchDirBttn));
+        directoryText = ((TextView)findViewById(R.id.directoryText));
 
         Intent intent = getIntent();
         String link = intent.getStringExtra(Intent.EXTRA_TEXT);
         if(link != null) {
             linkText.setText(link.toString());
             Toast.makeText(this,link.toString(),Toast.LENGTH_LONG).show();
-            download(savebttn);
+            findButton(findBttn);
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public void download(View v) {
+    public void findButton(View v) {
+        System.out.println(Environment.DIRECTORY_DOWNLOADS);
+        if(!linkText.getText().toString().contains("playlist")) {
+            getVideo();
+        }
+        else {
+            getPlayist();
+        }
+
+    }
+
+    public void getVideo() {
         YoutubeDownloader downloader = new YoutubeDownloader();
 
         downloader.addCipherFunctionPattern(2, "\\b([a-zA-Z0-9$]{2})\\s*=\\s*function\\(\\s*a\\s*\\)\\s*\\{\\s*a\\s*=\\s*a\\.split\\(\\s*\"\"\\s*\\)");
@@ -92,23 +113,22 @@ public class MainActivity extends AppCompatActivity {
             video = downloader.getVideo(videoID);
 
             // get videos with audio
-            List<AudioVideoFormat> videoWithAudioFormats = video.videoWithAudioFormats();
-//            List<String> videoWithAudioFormatsString = new LinkedList<String>();
-//            for(AudioVideoFormat videoFormat : videoWithAudioFormats) {
-//                videoWithAudioFormatsString.add(videoFormat.qualityLabel());
-//            }
+            videoWithAudioFormats = video.videoWithAudioFormats();
+            List<String> videoWithAudioFormatsString = new LinkedList<String>();
+            for(AudioVideoFormat videoFormat : videoWithAudioFormats) {
+                videoWithAudioFormatsString.add(videoFormat.qualityLabel());
+            }
             // Create an ArrayAdapter using the string array and a default spinner layout
-            ArrayAdapter<AudioVideoFormat> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, videoWithAudioFormats);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, videoWithAudioFormatsString);
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             // Apply the adapter to the spinner
             videosResolution.setAdapter(adapter);
-            videosResolution.setVisibility(View.VISIBLE);
 
             videosResolution.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    qualityPosition[0] = (AudioVideoFormat) parent.getSelectedItem();
+                    qualityPosition[0] = (int) parent.getSelectedItemPosition();
                 }
 
                 @Override
@@ -117,15 +137,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-            String videoQuality = videosResolution.getSelectedItem().toString();
-            System.out.println("Qualidade do vídeo: " + videoQuality);
-
-            videoURL = videoWithAudioFormats.get(videoWithAudioFormats.size()-1).url();
-            System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA "+videoURL);
-
             details = video.details();
+
             textView.setText(details.title());
             textView.setVisibility(View.VISIBLE);
+            saveButton.setVisibility(View.VISIBLE);
+            videosResolution.setVisibility(View.VISIBLE);
+            searchDirBttn.setVisibility(View.VISIBLE);
+            directoryText.setVisibility(View.VISIBLE);
             linkText.setText("");
 
 
@@ -133,10 +152,9 @@ public class MainActivity extends AppCompatActivity {
             textView.setText("Link corrompido");
             textView.setVisibility(View.VISIBLE);
         }
-
     }
 
-    public void getPlayist(View v) throws YoutubeException {
+    public void getPlayist() {
         YoutubeDownloader downloader = new YoutubeDownloader();
 
         downloader.addCipherFunctionPattern(2, "\\b([a-zA-Z0-9$]{2})\\s*=\\s*function\\(\\s*a\\s*\\)\\s*\\{\\s*a\\s*=\\s*a\\.split\\(\\s*\"\"\\s*\\)");
@@ -146,11 +164,15 @@ public class MainActivity extends AppCompatActivity {
 
         String playlistID = linkText.getText().toString();
         playlistID = playlistID.replace("https://youtube.com/playlist?list=", "");
-        YoutubePlaylist playlist = downloader.getPlaylist(playlistID);
+        try {
+            YoutubePlaylist playlist = downloader.getPlaylist(playlistID);
+        } catch (YoutubeException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public boolean download2(String path, String title, String url) {
+    public boolean downloadVideo(String title, String url) {
 
         if (Build.VERSION.SDK_INT >= 23) {
             if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -169,7 +191,7 @@ public class MainActivity extends AppCompatActivity {
                         .setTitle(title)
                         .setDescription("Downloading")
                         .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                        .setDestinationInExternalPublicDir(path, title)
+                        .setDestinationInExternalPublicDir(pathToDownload, title)
                         .setAllowedOverMetered(true)
                         .setAllowedOverRoaming(true);
 
@@ -181,19 +203,54 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.e("Permission error","You have asked for permission");
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                return download2(path, title, url);
+                return downloadVideo(title, url);
             }
         }
         else { //you dont need to worry about these stuff below api level 23
             Log.e("Permission error","You already have the permission");
-            return download2(path, title, url);
+            return downloadVideo(title, url);
         }
 
 
     }
 
-    public void downloadButtonDownload(View v) {
-        download2(Environment.DIRECTORY_DOWNLOADS, details.title(), videoURL);
+    public void downloadButton(View v) {
+        videoURL = videoWithAudioFormats.get(qualityPosition[0]).url();
+        downloadVideo(details.title(), videoURL);
+    }
+
+    public void openDirectory(View v) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
+            Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            i.addCategory(Intent.CATEGORY_DEFAULT);
+            startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 9999:
+                Uri uri = data.getData();
+                File file = new File(uri.getPath());
+                final String[] split = file.getPath().split(":");//split the path.
+                pathToDownload = split[1];//assign it to a string(your choice).
+                directoryText.setText(pathToDownload);
+                break;
+        }
+    }
+
+    public String parsePath(String path) {
+        String[] pathAux = path.split("%3A");
+//        if(pathAux[1].contains("%2F")) {
+//            pathAux[1]
+//        }
+        return "";
+
     }
 
 }
